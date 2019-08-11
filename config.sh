6e1c55e4270097c3c79ec8c3da842bdb0b868502
Jarl-Penguin <jarlpenguin@outlook.com>
2019-08-11 15:12:23 +0300

    Montana-ify
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="JarlPenguin"
+export GITHUB_EMAIL="jarlpenguin@gmail.com"
 
-export device=""
+export device="montana"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="LineageOS"
+export ROM_DIR="/var/lib/jenkins/los15"
+export ROM_VERSION="15.1"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://github.com/JarlPenguin/manifests/raw/master/los/15.1/montana.xml"
+export manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage"
+export branch="lineage-15.1"
 export bacon="bacon"
 export buildtype=""
 export clean=""
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="JarlPenguin/releases"
 
-export timezone="UTC"
+export timezone="Seoul"
