bab0f16d018230a62d4c0bf74feddebe7fd41530
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-17 04:34:02 +0300

    build: TWRP: our tags should not be recovery all the time
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -17,22 +17,23 @@ BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 
 export finalimg_path=$(ls "${outdir}/recovery.img" | tail -n -1)
-export img_name=$(echo "${finalimg_path}" | sed "s|${outdir}/||")
-export tag=$( echo "${img_name}" | sed 's|.img||')
+export tag="twrp-${device}-$(date +%m%d%Y-%H%m)"
 if [ -e "${finalimg_path}" ]; then
     echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
+
+    cp "${outdir}/recovery.img" "${outdir}/${tag}.img"
 
     echo "Uploading"
 
     github-release "${release_repo}" "${tag}" "master" "TWRP for ${device}
 
-Date: $(env TZ="${timezone}" date)" "${finalimg_path}"
+Date: $(env TZ="${timezone}" date)" "${outdir}/${tag}.img"
 
     echo "Uploaded"
 
     telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 
-Download: ["${img_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${img_name}")"
+Download: ["${tag}.img"]("https://github.com/${release_repo}/releases/download/${tag}/${tag}.img")"
 curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
 
 else
