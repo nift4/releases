8280c65648218907df0fe1e0a973f05b7f4de1f2
nift4 <nift4@protonmail.com>
2020-12-16 11:43:35 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
-
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Mint-OS"
+export ROM_DIR="${WORKSPACE}/MintOS"
+export ROM_VERSION="2.1"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://github.com/nift4/Mint-OS/raw/master/99-mintos.xml"
+export manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage"
+export branch="lineage-17.1"
 export bacon="bacon"
 export buildtype=""
-export clean=""
-export upload_recovery=""
+export clean="installclean"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="nift4/releases"
 
-export timezone="UTC"
+export timezone="GMT+1"
